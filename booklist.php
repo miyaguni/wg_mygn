8b706a795c947ddf3e0cc7de4ac3388c9c08e430
Seiya Miyaguni <www.mygn.com@gmail.com>
2015-01-10 19:06:59 +0900

    booklist
    でーた

---
+++ booklist.php
@@ -0,0 +1,41 @@
+<?php 
+
+$db = new mysqli("localhost", "petermac", "password", "library");
+
+// 上で設定した接続情報が正しいことを確認する
+if ($db->connect_error) {
+	die("Connect Error ({$db->connect_errno}) {$db->connect_error} ");
+}
+
+$sql = "SELECT * FROM books WHERE available = 1 ORDER BY title";
+$result = $db->query($sql);
+
+?>
+
+<html>
+<body>
+
+<table cellSpacing="2" cellPadding="6" align="center" border="1">
+	<tr>
+		<td colspan="4">
+			<h3 align="center">These Books are currently avairable</h3>
+		</td>
+	</tr>
+
+	<tr>
+		<td align="center">Title</td>
+		<td align="center">Year Published</td>
+		<td align="center">ISBN</td>
+	</tr>
+
+	<?php while ($show = $result->fetch_assoc()) { ?>
+		<tr>
+			<td><?php echo stripslashes($row['title']); ?></td>
+			<td align="center"><?php echo $row['pub_year']; ?></td>
+			<td><?php echo $row['ISBN']; ?></td>
+		</tr>
+	<?php } ?>
+</table>
+
+</body>
+</html>
