3f2e7f25ae2f8a92aa482ee416430483043ac022
Seiya Miyaguni <www.mygn.com@gmail.com>
2015-01-13 01:32:56 +0900

    omikuji
    ちょっと改良版

--- omikuji.php
+++ omikuji.php
@@ -1,12 +1,19 @@
 <?php
 
 	// array は 配列
-	$lucky = array("大吉", "中吉", "小吉", "凶");
+	$lucky = array("大吉", "中吉", "小吉");
+	$LUCKY = array("大吉", "中吉", "凶", "大凶");
 
 	// shuffle で配列の順番をシャッフルしている
 	shuffle($lucky);
 
 	// 配列luckyの[0]番目(自然数1)を表示
 	echo $lucky[0];
-	
+	echo "/";
+
+	// 配列から適当にひとつ取ってくる
+	$idx = array_rand($LUCKY);
+
+	echo $LUCKY[$idx];
+
 ?>
